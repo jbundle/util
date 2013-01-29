5aff8eb91c87e31b893f9fca63dc0c7ecd61d9a1
Don Corley <don@donandann.com>
2013-01-29 09:23:34 -0800

    Make sure cut and paste work in browser. Closes #39

--- muffinmanager/src/main/java/org/jbundle/util/muffinmanager/MuffinManager.java
+++ muffinmanager/src/main/java/org/jbundle/util/muffinmanager/MuffinManager.java
@@ -7,15 +7,27 @@
  */
 package org.jbundle.util.muffinmanager;
 
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
 
 import javax.jnlp.BasicService;
+import javax.jnlp.ClipboardService;
 import javax.jnlp.FileContents;
+import javax.jnlp.FileOpenService;
 import javax.jnlp.PersistenceService;
 import javax.jnlp.ServiceManager;
 import javax.jnlp.UnavailableServiceException;
+import javax.swing.Action;
+import javax.swing.JComponent;
+import javax.swing.text.JTextComponent;
+import javax.swing.text.TextAction;
 
 /** 
  * MuffinManager - This code handles the muffins for the java web start program.
@@ -168,4 +180,169 @@ public class MuffinManager extends Object
             return m_bs.showDocument(url);
         return false;
     }
+    private ClipboardService cs = null;
+    
+    public static final int CLIPBOARD_ENABLED = 1 << 0;
+    public static final int CLIPBOARD_DISABLED = 1 << 1;
+
+    protected int clipboardReadStatus = 0;
+    protected int clipboardWriteStatus = 0;
+
+    /**
+     * Get data from the system clipboard.
+     * @return
+     */
+    public Transferable getClipboardContents()
+    {
+        if ((clipboardReadStatus & CLIPBOARD_DISABLED) == CLIPBOARD_DISABLED)
+            return null;    // Rejected it last time, don't ask again
+        clipboardReadStatus = CLIPBOARD_DISABLED;
+        if (cs == null)
+        {
+            try {
+                cs = (ClipboardService)ServiceManager.lookup("javax.jnlp.ClipboardService"); 
+            } catch (UnavailableServiceException e) { 
+                cs = null;
+            }
+        }
+
+        if (cs != null) { 
+            // get the contents of the system clipboard and print them 
+            Transferable tr = cs.getContents(); 
+            if (tr != null)
+                clipboardReadStatus = CLIPBOARD_ENABLED;
+            return tr;
+        }
+        return null;
+    }
+    /**
+     * Set the global clipboard contents.
+     * @param data
+     */
+    public boolean setClipboardContents(Transferable data)
+    {
+        if (data == null)
+            return false;
+        if ((clipboardWriteStatus & CLIPBOARD_DISABLED) == CLIPBOARD_DISABLED)
+            return false;    // Rejected it last time, don't ask again
+        clipboardWriteStatus = CLIPBOARD_ENABLED;
+        if (cs == null)
+        {
+            try { 
+                cs = (ClipboardService)ServiceManager.lookup("javax.jnlp.ClipboardService"); 
+            } catch (UnavailableServiceException e) { 
+                cs = null; 
+            }
+        }
+
+        if (cs != null)
+        { // set the system clipboard contents to a string selection 
+            try { 
+                cs.setContents(data);
+                clipboardWriteStatus = CLIPBOARD_ENABLED;
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace(); 
+            } 
+        }
+        return false;
+    }
+    
+    /**
+     * Replace the clipboard cut/cut/paste command with this command.
+     * @param component
+     * @param actionName
+     * @return
+     */
+    public boolean replaceClipboardAction(JComponent component, String actionName)
+    {
+        Action action = (Action)component.getActionMap().get(actionName);
+        if (action != null)
+            component.getActionMap().put(actionName, new LinkedClipboardAction(actionName, action));
+        return (action != null);
+        
+    }
+    /**
+     * Do cut/copy/paste from system clipboard, or if not possible, do it from local clipboard.
+     * @author don
+     *
+     */
+    class LinkedClipboardAction extends TextAction   // Action?
+    {
+        private static final long serialVersionUID = 1L;
+        Action linkedAction;
+        public LinkedClipboardAction(String name, Action linkedAction)
+        {
+            super(name);
+            this.linkedAction = linkedAction;
+        }
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JTextComponent target = getTextComponent(e);
+            String name = (String)this.getValue(Action.NAME);
+            if (target != null) {
+                {
+                    if ("paste".equals(name))
+                    {
+                        Transferable contents = getClipboardContents();
+                        if (contents != null) { 
+                            try {
+                                String s = (String)contents.getTransferData(DataFlavor.stringFlavor);
+                                if (s != null)
+                                {
+                                    target.replaceSelection(s);
+                                    return;
+                                }
+                            } catch (UnsupportedFlavorException e1) {
+                                e1.printStackTrace();
+                            } catch (IOException e1) {
+                                e1.printStackTrace();
+                            }
+                        }
+                    }
+                }
+                
+                linkedAction.actionPerformed(e);
+                
+                {
+                    if (("cut".equals(name)) || ("copy".equals(name)))
+                    {
+                        String data = target.getSelectedText();
+                        StringSelection ss = new StringSelection(data); 
+                        if (ss != null)
+                            setClipboardContents(ss);
+                    }
+                }
+            }
+        }
+    }
+    
+    FileOpenService fos = null;;
+    /**
+     * Open this file.
+     * @return
+     */
+    public InputStream openFileStream(String pathHint, String[] extensions)
+    {
+        if (fos == null)
+        {
+            try { 
+                fos = (FileOpenService)ServiceManager.lookup("javax.jnlp.FileOpenService"); 
+            } catch (UnavailableServiceException e) { 
+                fos = null; 
+            }
+        }
+
+        if (fos != null) { 
+            try { 
+                // ask user to select a file through this service 
+                FileContents fc = fos.openFileDialog(pathHint, extensions);
+                return fc.getInputStream();
+            } catch (Exception e) { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return null;
+    }
+
 }
